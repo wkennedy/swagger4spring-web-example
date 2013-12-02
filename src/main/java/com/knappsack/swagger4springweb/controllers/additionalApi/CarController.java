dad464d8f3805c53c634d241cda1263d17f3c5bf
wkennedy <will.kennedy@sparcedge.com>
2013-12-02 09:49:33 -0500

    Added ApiResponses

--- src/main/java/com/knappsack/swagger4springweb/controllers/additionalApi/CarController.java
+++ src/main/java/com/knappsack/swagger4springweb/controllers/additionalApi/CarController.java
@@ -5,6 +5,7 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CarController {
 
     @ApiOperation(value = "Find all cars", notes = "Get all cars currently available", httpMethod = "GET", response = Car.class)
-    @ApiResponse(code = 500, message = "Process error")
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "Process error"), @ApiResponse(code = 405, message = "Invalid input")})
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public
     @ResponseBody
